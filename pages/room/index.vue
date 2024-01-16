0fd0a826f4e99dbb9ee106708988b55441f53508
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-16 23:24:49 +0700

    feat: create room page

---
+++ pages/room/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <CommonLayout class="flex-col justify-center">
+    <RoomContent />
+    <RoomFooter />
+  </CommonLayout>
+</template>
+
+<script lang="ts" setup>
+import CommonLayout from '@/components/Layout/CommonLayout.vue'
+import RoomContent from '@/components/Room/RoomContent.vue'
+import RoomFooter from '@/components/Room/RoomFooter.vue'
+
+import { getStreamPreview } from '@/utils/rtc.util'
+
+onMounted(() => {
+  initiateRoom()
+})
+
+const initiateRoom = async () => {
+  getStreamPreview({})
+}
+</script>
