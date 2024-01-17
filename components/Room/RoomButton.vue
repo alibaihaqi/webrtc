afacc1e4edfbe906b4ab389a46fdc604f2e97b6b
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-17 12:42:04 +0700

    feat: add room button

---
+++ components/Room/RoomButton.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import type { IRoomActionButton } from '@/interfaces/room.interface'
+
+const props = defineProps<{
+  isActive: boolean
+  sources: IRoomActionButton[]
+}>()
+
+const generateData = computed(() => {
+  return props.sources.find((source) => source.isActive === props.isActive)
+})
+
+const emit = defineEmits(['onClick'])
+
+const onClickHandler = () => {
+  emit('onClick')
+}
+</script>
+
+<template>
+  <div
+    :class="`
+      w-24 sm:w-28 flex flex-col items-center gap-1 hover:bg-gray-700
+      pt-2 pb-1 px-4 sm:px-6 rounded-md
+    `"
+    @click="onClickHandler"
+  >
+    <nuxt-img
+      class="w-5"
+      :src="generateData?.iconPath"
+    />
+    <p class="text-xs text-gray-400">
+      {{ generateData?.title }}
+    </p>
+  </div>
+</template>
