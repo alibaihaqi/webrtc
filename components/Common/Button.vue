e7128fde1e86c5fe8cda19a42ae7834fecb7752e
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-17 12:42:49 +0700

    feat: add common button

---
+++ components/Common/Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <button
+    :class="`
+      py-1 rounded-md font-bold text-center
+      hover:brightness-105 hover:shadow-md
+      self-center w-full
+      ${ btnType ? `btn-${btnType}` : 'btn-primary' }
+      ${ size ? `s-${size}` : null }
+    `"
+    @click="onClickHandler"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string
+  btnType?: string
+  size?: 'default' | 'medium' | 'small'
+}>()
+
+const emit = defineEmits(['onClick'])
+
+const onClickHandler = () => {
+  emit('onClick')
+}
+</script>
+
+<style scoped>
+.btn-primary {
+  @apply bg-blue-500 text-white;
+}
+
+.btn-secondary {
+  @apply bg-white text-blue-500 border border-blue-500;
+}
+
+.btn-error {
+  @apply bg-white text-red-500 border border-red-500;
+}
+
+.s-default {
+  @apply max-w-[100%];
+}
+
+.s-medium {
+  @apply max-w-[50%];
+}
+.s-small {
+  @apply max-w-[25%];
+}
+</style>
