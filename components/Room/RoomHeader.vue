c737d6373a1dac65b02a9e679c080c35727523e4
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-17 12:40:11 +0700

    feat: create room header

---
+++ components/Room/RoomHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="self-center py-1 px-4 sm:px-8 mt-2 rounded-md">
+    <p class="text-sm text-white">
+      Room ID: <span class="hover:text-blue-400 hover:cursor-pointer">
+        {{ roomStore.roomId }}
+      </span>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRoomStore } from '@/stores/room.store'
+
+const roomStore = useRoomStore()
+</script>
