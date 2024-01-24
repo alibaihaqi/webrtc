7132b42884532d7ed2d7c55ba7b99ba5df6099f9
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-24 15:00:20 +0700

    feat: add rest util

---
+++ utils/rest.util.ts
@@ -0,0 +1,27 @@
+import type { IInitiateRoom } from '@/interfaces/room.interface'
+import { useRoomStore } from '@/stores/room.store'
+
+export const checkAvailabilityRoom = async (payload: IInitiateRoom) => {
+  const config = useRuntimeConfig()
+  const roomStore = useRoomStore()
+  const { data, error } = await useFetch(`${config.public.apiDomain}/rtc/check-room`, {
+    lazy: true,
+    server: false,
+    method: 'POST',
+    body: {
+      roomId: payload.roomId,
+    },
+  })
+
+  if (error.value) {
+    console.log('Error:', error.value)
+
+    return {
+      success: false,
+    }
+  }
+
+  roomStore.setIsHostMeeting(false)
+  roomStore.setRoomId(payload.roomId as string)
+  return data.value
+}
