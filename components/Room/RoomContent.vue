8a1d7ad721ac5a09d117897bd04a9680fd17d7a0
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-02-04 21:03:29 +0900

    feat: add room participants components

--- components/Room/RoomContent.vue
+++ components/Room/RoomContent.vue
@@ -2,14 +2,26 @@
   <div class="flex flex-1 bg-gray-800 text-white">
     <section class="flex flex-col flex-1">
       <RoomHeader />
-    
+
       <section id="videos_container" />
+    </section>
+
+    <section class="flex flex-col">
+      <RoomParticipants
+        v-if="roomStore.isShowParticipants"
+        :room-users="roomStore.roomUsers"
+        :set-is-show-participants="roomStore.setIsShowParticipants"
+      />
     </section>
   </div>
 </template>
 
 <script lang="ts" setup>
 import RoomHeader from '@/components/Room/RoomHeader.vue'
+import RoomParticipants from '@/components/Room/RoomParticipants.vue'
+import { useRoomStore } from '@/stores/room.store'
+
+const roomStore = useRoomStore()
 </script>
 
 <style>
