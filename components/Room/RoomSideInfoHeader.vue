326d2c143e62f96fae0788632ee8f07786235ff6
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-02-04 21:03:59 +0900

    feat: add room side info header

---
+++ components/Room/RoomSideInfoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="flex items-center py-2 px-4">
+    <p class="flex-1 text-gray-100 text-sm text-center">
+      {{ title }}
+    </p>
+
+    <button
+      class="py-1 px-4 bg-red-600 text-xs rounded-md"
+      @click="btnAction"
+    >
+      Close
+    </button>
+  </section>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string
+  btnAction: any
+}>()
+</script>
