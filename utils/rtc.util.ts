b0e936c1553cf2d7290b39d71277fd506eef3cc8
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-02-14 13:45:31 +0700

    feat: update rtc util

--- utils/rtc.util.ts
+++ utils/rtc.util.ts
@@ -24,7 +24,7 @@ export const getStreamPreview = async () => {
   try {
     localStream = await openMediaDevices(defaultMediaStreamConstraints)
     
-    showVideoStream(localStream, roomStore.socketId, true, 0)
+    showVideoStream(localStream, roomStore.socketId, 0)
 
     roomStore.isHostMeeting
       ? wsCreateRoom(username)
@@ -45,7 +45,7 @@ const getConnectedDevices = async (type: string) => {
   return devices.filter(device => device.kind === type)
 }
 
-export const showVideoStream = (stream: MediaStream, socketId: string = '', isMuteVideo: boolean = true, videoVolume: number = 1) => {
+export const showVideoStream = (stream: MediaStream, socketId: string = '', videoVolume: number = 1) => {
   const videosContainer = document.getElementById('videos_container')
   videosContainer?.classList.add('videos_container_styles')
 
@@ -56,7 +56,7 @@ export const showVideoStream = (stream: MediaStream, socketId: string = '', isMu
   videoElement.autoplay = true
   videoElement.playsInline = true // to disable iPhone trigger full screen
   videoElement.volume = videoVolume
-  videoElement.muted = isMuteVideo
+  videoElement.muted = true
   videoElement.srcObject = stream
 
   if (socketId) {
@@ -126,7 +126,7 @@ export const handleSignalingData = (data: any) => {
 }
 
 const addStream = (stream: MediaStream, connectedSocketId: string) => {
-  showVideoStream(stream, connectedSocketId, false)
+  showVideoStream(stream, connectedSocketId)
 }
 
 export const concatNewMessage = (message: any) => {
